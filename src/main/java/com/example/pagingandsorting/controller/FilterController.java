ab87f1faa4effd26a8e820655515328e9731d4af
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-04-04 13:54:54 +0300

    introduce specification, next time, first thing to do is to insert data in db

--- src/main/java/com/example/pagingandsorting/controller/FilterController.java
+++ src/main/java/com/example/pagingandsorting/controller/FilterController.java
@@ -3,11 +3,13 @@ package com.example.pagingandsorting.controller;
 import com.example.pagingandsorting.entety.Student;
 import com.example.pagingandsorting.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @RestController
@@ -33,5 +35,22 @@ public class FilterController {
     public List<Student> getStudentBySubject(@PathVariable String sub){
 
         return studentRepo.findBySubjectsName(sub);
+    }
+
+    @PostMapping("/specification")
+    public List<Student> getStudentSpecif(){
+
+        Specification<Student> specification = new Specification<>() {
+
+            @Override
+            public Predicate toPredicate(Root<Student> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get("name"), "Cosmin");
+                // vrem toti studentii care au numele = "Cosmin"
+            }
+        };
+
+        List<Student> all = studentRepo.findAll(specification);
+
+        return all;
     }
 }
--- src/main/java/com/example/pagingandsorting/repo/StudentRepo.java
+++ src/main/java/com/example/pagingandsorting/repo/StudentRepo.java
@@ -2,10 +2,11 @@ package com.example.pagingandsorting.repo;
 
 import com.example.pagingandsorting.entety.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface StudentRepo extends JpaRepository<Student, Long> {
+public interface StudentRepo extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
 
     Student findByName(String name);
 
